2be74cbbb7d06eaf5c2c24ac967fb4f5859c214b
Douglas Victor Menezes <dv@douglasvictor.com>
2022-05-16 19:33:22 -0300

    subindo a calculadora

---
+++ calculadora.js
@@ -0,0 +1,11 @@
+const somar = require('./somar')
+const subtrair = require('./subtrair')
+const multiplicar = require('./multiplicar')
+const dividir = require('./dividir')
+
+console.log(somar(2,3))
+console.log(subtrair(5,3))
+console.log(multiplicar(3,3))
+console.log(multiplicar(0,3))
+console.log(dividir(3,3))
+console.log(dividir(2,0))
---
+++ multiplicar.js
@@ -0,0 +1,9 @@
+function multiplicar (a,b){
+    if (a == 0  || b == 0 ){
+        return "Zero"
+    } else {
+        return a * b
+    }
+}
+
+module.exports = multiplicar;
